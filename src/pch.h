9fac0f78b5f2d381f25efe9db61dcb2e988b572e
ErenKarakas1 <69261887+ErenKarakas1@users.noreply.github.com>
2024-09-24 09:02:08 +0300

    build/optional PCH and change resource structure

---
+++ src/pch.h
@@ -0,0 +1,31 @@
+#ifndef PCH_H
+#define PCH_H
+
+#ifdef __cplusplus
+
+#include <QAbstractItemModel>
+#include <QCoreApplication>
+#include <QDebug>
+#include <QList>
+#include <QMetaObject>
+#include <QMetaType>
+#include <QMimeDatabase>
+#include <QModelIndex>
+#include <QModelIndexList>
+#include <QObject>
+#include <QPersistentModelIndex>
+#include <QQmlEngine>
+#include <QSqlDatabase>
+#include <QString>
+#include <QStringView>
+#include <QThread>
+#include <QTimer>
+#include <QUrl>
+#include <QVariant>
+
+#include <memory>
+#include <utility>
+
+#endif
+
+#endif // PCH_H
